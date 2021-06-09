d6b1a11c0a4b6f849da2dd583341585a4105f511
Onionie <gutierrez.onionie@gmail.com>
2021-06-08 17:10:05 -0700

    Hide API Key

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require('express');
 const https = require('https');
 const app = express();
 
+const apiKey = process.env.API_KEY;
+
 app.get("/", function(req, res){
+  https.get("https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + apiKey + "&units=metric");
   console.log("App is running");
 });
 
