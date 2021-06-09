0490bfbf471f33b107e1218a58a891a3a32b20c0
Onionie <gutierrez.onionie@gmail.com>
2021-06-08 20:14:04 -0700

    Fetch API

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ const app = express();
 const apiKey = process.env.API_KEY;
 
 app.get("/", function(req, res){
-  https.get("https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + apiKey + "&units=metric");
-  console.log("App is running");
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + apiKey + "&units=metric";
+  https.get(url, function(response){
+    console.log(response.statusCode);
+    response.on('data', function(data){       //callback data
+      const weatherData = JSON.parse(data); //parse the data into JSON
+      var weatherDesc = weatherData.weather[0].description;
+      console.log(weatherDesc);
+      res.send("The weather is  " + weatherDesc);
+    });
+
+  });
+
 });
 
 
